87cd299b6e8a22311dc5ab8cb1ec88d9c9b5e3ff
Ioan Lucut <ioan.lucut88@gmail.com>
2023-05-06 18:34:43 +0300

    Move to OUT

--- pptLyricsParser.ts
+++ pptLyricsParser.ts
@@ -9,6 +9,7 @@ const cleanOutputDirAndProcessFrom = (sourceDir: string, outputDir: string) => {
   fs.readdirSync(sourceDir).forEach((fileName: string) => {
     const filePath = path.join(sourceDir, fileName);
     const data = fs.readFileSync(filePath);
+    console.log(`Processing ${fileName}..`);
 
     const { exportFileName, basicTemplate } = processPPTFileAndConvertToTxt(
       data,
@@ -20,5 +21,9 @@ const cleanOutputDirAndProcessFrom = (sourceDir: string, outputDir: string) => {
 };
 
 (async () => {
-  cleanOutputDirAndProcessFrom('./txt_from_ppt_fc', './RAW_SOURCE_PPT_FROM_FC');
+  cleanOutputDirAndProcessFrom(
+    '/Users/ilucut/Biserica Emanuel SB SYNC/Cantece (PPT sau Audio)/PPT cor de copii Dynamis/PPT',
+    './out/cor_copii_ppt',
+  );
+  // cleanOutputDirAndProcessFrom('./RAW_SOURCE_PPT_FROM_FC', './txt_from_ppt_fc');
 })();
