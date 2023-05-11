a309ff59cf8862cd3e6dd9f6344d1418f34cbc39
Ioan Lucut <ioan.lucut88@gmail.com>
2023-05-11 15:28:52 +0300

    Songs

--- pptLyricsParser.ts
+++ pptLyricsParser.ts
@@ -15,9 +15,12 @@ const cleanOutputDirAndProcessFrom = async (
   fsExtra.emptyDirSync(outputDir);
 
   (await recursive(sourceDir, ['.DS_Store']))
-    .filter((filePath) => !path.basename(filePath).includes('Icon'))
-    // .filter((filePath) => path.basename(filePath).match(/ria/))
-    // .filter((filePath) => path.basename(filePath).includes('rege'))
+    .filter((filePath) => path.extname(filePath) === '.pptx')
+    .filter(
+      (filePath) =>
+        !path.basename(filePath).includes('Icon') &&
+        !path.basename(filePath).startsWith('$'),
+    )
     .forEach((filePath: string) => {
       const fileName = path.basename(filePath);
       const data = fs.readFileSync(filePath);
@@ -36,5 +39,60 @@ const cleanOutputDirAndProcessFrom = async (
   await cleanOutputDirAndProcessFrom(
     `${RAW_CLOUD_DATA}/Cor de copii Dynamis/PPT`,
     './out/cor_copii_ppt',
+  );
+
+  await cleanOutputDirAndProcessFrom(
+    `${RAW_CLOUD_DATA}/Cantari Beni`,
+    './out/cantari_beni_ppt',
+  );
+
+  await cleanOutputDirAndProcessFrom(
+    `${RAW_CLOUD_DATA}/Cantari fanfara`,
+    './out/cantari_fanfara_ppt',
+  );
+
+  await cleanOutputDirAndProcessFrom(
+    `${RAW_CLOUD_DATA}/Grupul Betel`,
+    './out/grupul_bethel__ppt',
+  );
+
+  await cleanOutputDirAndProcessFrom(
+    `${RAW_CLOUD_DATA}/Grupul Ekklesia`,
+    './out/grupul_ekklesia__ppt',
+  );
+
+  await cleanOutputDirAndProcessFrom(
+    `${RAW_CLOUD_DATA}/Grupul Petras`,
+    './out/grupul_petras__ppt',
+  );
+
+  await cleanOutputDirAndProcessFrom(
+    `${RAW_CLOUD_DATA}/Cor Barbatesc`,
+    './out/cor_barbatesc__ppt',
+  );
+
+  await cleanOutputDirAndProcessFrom(
+    `${RAW_CLOUD_DATA}/Cantari Florin/(florin colinde)`,
+    './out/fc_colinde__ppt',
+  );
+
+  await cleanOutputDirAndProcessFrom(
+    `${RAW_CLOUD_DATA}/Cantari Florin/(florin rusalii)`,
+    './out/fc_rusalii__ppt',
+  );
+
+  await cleanOutputDirAndProcessFrom(
+    `${RAW_CLOUD_DATA}/Cantari Florin/(florin staruinta)`,
+    './out/fc_staruinta__ppt',
+  );
+
+  await cleanOutputDirAndProcessFrom(
+    `${RAW_CLOUD_DATA}/Cantari Florin/[florin toate cantarile]`,
+    './out/fc_toate_cantarile__ppt',
+  );
+
+  await cleanOutputDirAndProcessFrom(
+    `${RAW_CLOUD_DATA}/Cantari Florin/Cantari cu LOGO-ul bisericii`,
+    './out/fc_cu_logo__ppt',
   );
 })();
